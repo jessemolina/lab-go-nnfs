709d2163768e954e24c3c59531604b994b1399fe
Jesse Molina <jesse@jessemolina.xyz>
2022-09-24 11:15:38 -0500

    updated functions and types

--- cmd/ann/ann.go
+++ cmd/ann/ann.go
@@ -2,6 +2,29 @@ package ann
 
 import "math/rand"
 
+// ================================================================
+// FUNCTIONS
+
+// create a neuron with random weights and bias
+// inputs defines the size of weights
+func NewRandomNeuron(inputs int) Neuron {
+	var w []float64
+	for i := 0; i < inputs; i++ {
+		w = append(w, rand.Float64())
+	}
+	return Neuron{w, 0}
+}
+
+// TODO create a random layer
+// num of nuerons, num of dimmensions (len of inputs)
+func NewRandomLayer(outputs int) Layer {
+	neurons := make([]*Neuron, outputs)
+	return Layer{neurons}
+}
+
+// ================================================================
+// TYPES - Neuron
+
 type Neuron struct {
 	Weights []float64
 	Bias    float64
@@ -17,11 +40,10 @@ func (n *Neuron) Predict(x []float64) []float64 {
 	return []float64{z + n.Bias}
 }
 
-// create a neuron with zero bias and random weights in d dimmension
-func NewRandomNeuron(d int) Neuron {
-	var w []float64
-	for i := 0; i < d; i++ {
-		w = append(w, rand.Float64())
-	}
-	return Neuron{w, 0}
+
+// ================================================================
+// TYPES - Layer
+
+type Layer struct {
+	Neurons []*Neuron
 }
--- cmd/ann/ann_test.go
+++ cmd/ann/ann_test.go
@@ -6,27 +6,56 @@ import (
 	"github.com/jessemolina/lab-go-nnfs/cmd/ann"
 )
 
+// ================================================================
+// TEST VARIABLES
 // TODO write table driven testing - create variations of n to test against
 
-var n = ann.Neuron{[]float64{1, 2, 3},2}
+// inputs - 4 input features
+var x = []float64{1.0, 2.0, 3.0, 2.5}
 
-// test (*Neuron).Predict()
-func TestPredict(t *testing.T) {
-	results := n.Predict([]float64{0.2, 0.8, -0.5})
-	expected := []float64{2.4}
-	if results[0] != expected[0] {
-		t.Errorf("TestPredict\nexpected:%f\nresults:%f\n", expected, results)
-	}
+// weights - 3 output neurons
+var w = [][]float64{
+	{0.2, 0.8, -0.5, 1},
+	{0.5, -0.91, 0.26, -0.5},
+	{-0.26, -0.27, 0.17, 0.87},
 }
 
-// test NewRandonNeurdon(d int)
+// bias - 3 weight biases
+var b = []float64{2, 3, 0.5}
+
+// neuron - 4 inputs features
+var n = ann.Neuron{w[0], b[0]}
+
+// layer - 3 output neurons
+var l = ann.Layer{
+	Neurons: make([]*ann.Neuron, len(w)),
+}
+
+// ================================================================
+// TEST FUNCTIONS
+
+// test NewRandonNeuron(d int)
 func TestNewRandomNeuron(t *testing.T) {
-	d := 3
-	n := ann.NewRandomNeuron(d)
+	n := ann.NewRandomNeuron(len(x))
 	results := len(n.Weights)
-	expected := d
+	expected := len(x)
 
 	if results != expected {
 		t.Errorf("TestNewRandomNeuron\nexpected:%d\nresults:%d\n", expected, results)
+	}
+}
+
+// TODO test NewRandomLayer(outputs int)
+
+// ================================================================
+// TEST METHODS
+
+
+// test (*Neuron).Predict()
+func TestPredict(t *testing.T) {
+	results := n.Predict(x)
+	expected := []float64{4.8}
+	if results[0] != expected[0] {
+		t.Errorf("TestPredict\nexpected:%f\nresults:%f\n", expected, results)
 	}
 }
